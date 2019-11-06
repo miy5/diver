b8f1fd19bfd3ccdfd08fa08f29a2d8ab93df07ef
miy5 <miy5alice@gmail.com>
2019-11-06 12:04:11 +0000

    [Add] post_image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '67c0aaca4258ac5e44790a5d67a9f335293c6e52a47d79e2432f1da5bc5236a6b509a00c6b2b6a8e9e0f04b31993cea48a6f8eb22dc475a82560fff8ad6ca587'
