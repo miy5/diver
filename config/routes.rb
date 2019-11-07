3b45cef0497081b16723cf62356850b0b1829627
miy5 <miy5alice@gmail.com>
2019-11-07 06:06:53 +0000

    [Add] post_comments,favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
-  resources :post_images
+  resources :post_images do
+  	resource :favorites, only: [:create, :destroy, :index]
+  	resource :post_comments, only: [:create, :destroy, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
